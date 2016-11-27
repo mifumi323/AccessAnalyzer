158bafd5858b9bc31543e4e98ae29d1bf45b1592
mifumi323 <mifumi323@gmail.com>
2016-11-27 23:14:19 +0900

    RefererAnalyzer

---
+++ AccessAnalyzerSample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MifuminLib.AccessAnalyzer;
+
+namespace AccessAnalyzerSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RefererAnalyzer.GetSearchPhrase("https://www.google.co.jp/search?q=%E3%82%A2%E3%83%B3%E3%83%91%E3%83%B3%E3%83%9E%E3%83%B3&ie=utf-8"));
+        }
+    }
+}
