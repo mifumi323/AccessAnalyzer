915127ad535609515538a88b370084b4295822c6
mifumi323 <mifumi323@gmail.com>
2022-01-12 08:19:06 +0900

    LogReaderクラス追加
    #4

---
+++ LogReader/LogReader.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LogReader
+{
+    public class LogReader:IDisposable
+    {
+        private Stream owningStream;
+        private StreamReader streamReader;
+        private bool disposedValue;
+
+        public LogReader(Stream stream)
+        {
+            streamReader = new StreamReader(stream);
+        }
+
+        public LogReader(string path):this(File.OpenRead(path))
+        {
+            owningStream = streamReader.BaseStream;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    streamReader = null;
+                }
+
+                // 自分で確保した場合のみ破棄する
+                if (owningStream != null)
+                {
+                    owningStream.Dispose();
+                    owningStream = null;
+                }
+                disposedValue = true;
+            }
+        }
+
+        ~LogReader()
+        {
+            // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
